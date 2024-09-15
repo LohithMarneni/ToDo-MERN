6eab199865db715410d18a09a08db6086df448cb
LohithMarneni <lohith.marneni@gmail.com>
2024-09-15 12:03:52 +0530

    changed profile page

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -4,12 +4,11 @@ import { Context } from '../main';
 import Loader from '../components/Loader';
 const Profile = () => {
   const {isAuthenticated,loading,user}=useContext(Context);
-
   return (
     loading?<Loader/>:
       <div>
-      <h1>{user?.name}</h1>
-      <h1>{user?.email}</h1>
+      <h1>{isAuthenticated ? user.name : 'Not Authenticated'}</h1>
+      <h1>{isAuthenticated ? user.email : 'N/A'}</h1>
     </div>
   )
 }
